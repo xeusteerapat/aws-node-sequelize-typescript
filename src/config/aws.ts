cbfce919f1818a186f70b8f8b498638d44f6f365
xeusteerapat <xeus085@gmail.com>
2021-03-15 15:43:09 +0700

    [ADD] aws config for url signer

---
+++ src/config/aws.ts
@@ -0,0 +1,52 @@
+import AWS = require('aws-sdk');
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { AWS_PROFILE, AWS_REGION, AWS_MEDIA_BUCKET } = process.env;
+
+//Configure AWS
+var credentials = new AWS.SharedIniFileCredentials({ profile: AWS_PROFILE });
+AWS.config.credentials = credentials;
+
+export const s3 = new AWS.S3({
+  signatureVersion: 'v4',
+  region: AWS_REGION,
+  params: { Bucket: AWS_MEDIA_BUCKET },
+});
+
+/* getGetSignedUrl generates an aws signed url to retreive an item
+ * @Params
+ *    key: string - the filename to be put into the s3 bucket
+ * @Returns:
+ *    a url as a string
+ */
+export function getGetSignedUrl(key: string): string {
+  const signedUrlExpireSeconds = 60 * 5;
+
+  const url = s3.getSignedUrl('getObject', {
+    Bucket: AWS_MEDIA_BUCKET,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+
+  return url;
+}
+
+/* getPutSignedUrl generates an aws signed url to put an item
+ * @Params
+ *    key: string - the filename to be retreived from s3 bucket
+ * @Returns:
+ *    a url as a string
+ */
+export function getPutSignedUrl(key: string) {
+  const signedUrlExpireSeconds = 60 * 5;
+
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: AWS_MEDIA_BUCKET,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+
+  return url;
+}
