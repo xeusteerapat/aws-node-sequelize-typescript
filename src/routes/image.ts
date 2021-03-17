80ee1425b61dc26e139c8edb6fa4fd749826f6a6
xeusteerapat <xeus085@gmail.com>
2021-03-17 17:31:45 +0700

    [EDIT] update new image route

--- src/routes/image.ts
+++ src/routes/image.ts
@@ -3,11 +3,13 @@ import { imageUpload } from '../controllers/image';
 
 const router = Router();
 import multer from 'multer';
+import { uploadFileToAws } from '../config/aws';
 
 const uploadFile = multer({
   storage: multer.memoryStorage(),
 }).single('file');
 
 router.post('/image/upload', uploadFile, imageUpload);
+router.post('/image/aws', uploadFile, uploadFileToAws);
 
 export default router;
