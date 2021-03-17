5148ecb1e0146f32118100409bcd4264c3f8a1ee
xeusteerapat <xeus085@gmail.com>
2021-03-17 17:32:15 +0700

    [EDIT] update image upload handler

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Response, Request } from 'express';
 import AWS = require('aws-sdk');
 import dotenv from 'dotenv';
 
@@ -12,7 +13,7 @@ AWS.config.credentials = credentials;
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: AWS_REGION,
-  params: { Bucket: AWS_MEDIA_BUCKET },
+  // params: { Bucket: AWS_MEDIA_BUCKET },
 });
 
 /* getGetSignedUrl generates an aws signed url to retreive an item
@@ -49,4 +50,47 @@ export function getPutSignedUrl(key: string) {
   });
 
   return url;
+}
+
+// ### Another way to upload file ### //
+const S33 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+});
+
+export async function uploadFileToAws(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const imagemin = require('imagemin');
+
+  const { buffer } = req.file;
+
+  const image = await imagemin.buffer(buffer);
+
+  const params = {
+    Bucket: AWS_MEDIA_BUCKET as string,
+    Key: req.file.originalname,
+    Body: image,
+    ContentType: 'image/png',
+    ACL: 'public-read',
+  };
+
+  interface UploadSuccess {
+    ETag: string;
+    ServerSideEncryption: string;
+    Location: string;
+    key: string;
+    Key: string;
+    Bucket: string;
+  }
+
+  const uploadResult: UploadSuccess = await new Promise((resolve, reject) => {
+    S33.upload(params, (err: any, data: any) =>
+      err == null ? resolve(data) : next(reject(err))
+    );
+  });
+
+  res.send({ uploadUrl: uploadResult.Location });
 }
