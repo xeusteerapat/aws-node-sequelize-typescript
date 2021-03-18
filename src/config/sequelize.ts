6db23139512c0cf770d57501331c6590439b3925
xeusteerapat <xeus085@gmail.com>
2021-03-18 15:51:43 +0700

    [EDIT] update config port

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -9,6 +9,7 @@ export const sequelize = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   dialect: 'postgres',
   storage: ':memory:',
 });
